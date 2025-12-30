a47776f4f133a4d8bb833748bff9f44b91165ad6
Niyaj Kumanali <nkumanali@Lenovo.com>
2025-12-30 09:31:09 +0530

    updated the components into smaller componenets and done small css changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import './App.scss'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import { getUserMenuState } from './utility/userMenu'
-import { useAppDispatch } from './store/hooks'
-import { useEffect } from 'react'
 
 function App() {
 
-  const isUserMenuOpen = getUserMenuState()
-  const dispatch = useAppDispatch();
-
-  
+ 
   return (
     <>
       <Navbar />
--- src/utility/userMenu.ts
+++ src/utility/userMenu.ts
@@ -1,5 +1,4 @@
 import { useAppSelector } from "../store/hooks"
-import { userMenuActions } from "../store/UserMenu/usermenu.slice";
 
 export function getUserMenuState() {
     const isUserMenuOpen = useAppSelector(state => state.userMenu.isUserMenuOpen);
